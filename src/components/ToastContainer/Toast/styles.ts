cf2413964a5fe2cfc9ce530d0dfcc1bf6900fb05
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-08 18:31:21 -0300

    add animation to the toast using react-spring

--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -1,3 +1,4 @@
+import { animated } from 'react-spring';
 import styled, { css } from 'styled-components';
 
 interface ContainerProps {
@@ -20,7 +21,7 @@ const toastTypeVariations = {
   `,
 };
 
-export const Container = styled.div<ContainerProps>`
+export const Container = styled(animated.div)<ContainerProps>`
   position: relative;
   width: 360px;
   padding: 16px 30px 16px 16px;
