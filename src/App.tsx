cf581c76080eb530cc69fe08ee54b1fab5c1bb76
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-08 18:56:08 -0300

    create the app routes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import AppProvider from './hooks';
 
 import GlobalStyle from './styles/global';
 
-import SignIn from './pages/SignIn';
-
-import AppProvider from './hooks';
-
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
 
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 };
 
